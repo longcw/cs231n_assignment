b7e0f4582f5357d047b722080a75bbb898d39ac2
longc-surfacebook <longch1024@gmail.com>
2016-09-27 15:19:23 +0800

    linear svm gradient decent

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -49,7 +49,11 @@ class LinearClassifier(object):
             # Hint: Use np.random.choice to generate indices. Sampling with         #
             # replacement is faster than sampling without replacement.              #
             #########################################################################
-            pass
+            indices = np.random.choice(num_train, batch_size)
+            X_batch = X[indices]
+            y_batch = y[indices]
+            # X_batch = X_batch.T
+            y_batch = np.reshape(y_batch, (batch_size,))
             #########################################################################
             #                       END OF YOUR CODE                                #
             #########################################################################
@@ -63,7 +67,7 @@ class LinearClassifier(object):
             # TODO:                                                                 #
             # Update the weights using the gradient and the learning rate.          #
             #########################################################################
-            pass
+            self.W += -learning_rate * grad
             #########################################################################
             #                       END OF YOUR CODE                                #
             #########################################################################
@@ -91,7 +95,8 @@ class LinearClassifier(object):
         # TODO:                                                                   #
         # Implement this method. Store the predicted labels in y_pred.            #
         ###########################################################################
-        pass
+        S = np.matmul(X, self.W)
+        y_pred = np.argmax(S, axis=1)
         ###########################################################################
         #                           END OF YOUR CODE                              #
         ###########################################################################
